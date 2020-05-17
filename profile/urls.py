d581ad5b5ac20598fc6ba3130e1ab633a2f5c9fe
mohammadali <seifkashani14@gmail.com>
2020-05-18 04:26:02 +0430

    middle of login

--- profile/urls.py
+++ profile/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:userID>/profile/', views.profile, name='profile'),
+    path('<int:userID>/update/', views.update, name='update'),
 ]
--- profile/views.py
+++ profile/views.py
@@ -1,4 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
-def index():
-    pass
+from .models import User
+
+
+def index(request):
+    return render(request, 'profile/index.html', {})
+
+
+def profile(request, userID):
+    user = User.objects.get(id=userID)
+    output = ''
+    for attribute in dir(user):
+        if not '_' in attribute:
+            output += attribute + '\n'
+    return HttpResponse(output)
+
+
+def update(request, userID):
+    return HttpResponse('to update page')
